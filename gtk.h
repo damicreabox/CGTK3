8ba5fb40d489a6d41978fabaf74a95890942a130
Dami <dami@creabox.net>
2016-10-07 22:49:35 +0200

    Update connect.

--- gtk.h
+++ gtk.h
@@ -5,9 +5,9 @@ typedef int pid_t;
 
 #include <gtk/gtk.h>
 
-typedef void  (*SGGCallback)(GtkWidget*,gpointer);
-gulong gtk_connect(GtkWidget *widget, const char * signal, SGGCallback callback, gpointer * data, GConnectFlags flags) {
-	return g_signal_connect_data (widget, signal, G_CALLBACK (callback), data, NULL, flags);
+typedef void  (*SGGCallback)(GtkWidget*,void *);
+gulong gtk_connect(GtkWidget *widget, const char * signal, SGGCallback callback, void * data) {
+	return g_signal_connect(widget, signal, G_CALLBACK (callback), data);
 }
 
 #endif
